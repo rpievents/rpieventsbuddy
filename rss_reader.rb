a3422c9dcaf30534b7c4e882b93fe16b77c5ae4a
Eric Cunningham <ejcunningham@gmail.com>
2009-04-16 11:06:06 -0400

    Fixed parsing.  Works on my machine.  Also fixed date problem

--- rss_reader.rb
+++ rss_reader.rb
@@ -1,20 +1,53 @@
+#!/usr/bin/env ruby
+require 'rubygems'
 require 'rss/1.0'
 require 'rss/2.0'
 require 'open-uri'
+#require 'dbi'
+
+#************************
+#* CONNECTS TO POSTGRES *
+#************************
+#puts "\nConnecting to Postgres..."
+#dbh = DBI.connect('DBI:Pg:database=test;host=localhost', 'postgres', 'tennis')
+#puts "Connection successful.\n"
+#************************
 
 class Event
-  @Title = ""
-  @Date = ""
-  @Time = ""
-  @Description = ""
-  @Link = ""
-  attr_accessor :Title, :Date, :Time, :Description, :Link
-  def initialize(title,date,time,description,link)
+    @Title
+    @Location
+    @Start
+    @End
+    @Date
+    @Host
+    @Contact
+    @Website
+    @Description
+    @Creator
+  attr_accessor :Title, :Location, :Start, :End, :Date, :Host, :Contact, :Website, :Description, :Creator
+  def initialize(title, location, start, end1, date, host, contact, website, description, creator)
+    @Title = ""
+    @Location = ""
+    @Start = ""
+    @End = ""
+    @Date = ""
+    @Host = ""
+    @Contact = ""
+    @Website = ""
+    @Description = ""
+    @Creator = ""
+  end
+  def set_info(title, location, start, end1, date, host, contact, website, description, creator)
     @Title = title
+    @Location = location
+    @Start = start
+    @End = end1
     @Date = date
-    @Time = time
-    @Desciption = description
-    @Link = link
+    @Host = host
+    @Contact = contact
+    @Website = website
+    @Description = description
+    @Creator = creator
   end
 end
 
@@ -41,20 +74,22 @@ while events_string.match(/<item>(.+?)<\/item>/)
   title[/<title>/] = ''
   title[/<\/title>/] = ''
   new_title = title.split(" - ")
-  date = new_title[1]
+  #date = new_title[1]
   title = new_title[0]
   #parse link
-  link = events_string[/<link>(.+?)<\/link>/]
+  website = events_string[/<link>(.+?)<\/link>/]
   events_string[/<link>(.+?)<\/link>/] = ""
-  link[/<link>/] = ''
-  link[/<\/link>/] = ''
+  website[/<link>/] = ''
+  website[/<\/link>/] = ''
   #parse description
   description = events_string[/<description>(.+?)<\/description>/]
   events_string[/<description>(.+?)<\/description>/] = ""
   description[/<description>/] = ''
   description[/<\/description>/] = ''
   #puts description
-
+  #parse date
+  date = description[/, (.+?)20\d\d/]
+  date[0,2]=''
   #parse time
   stime = description[/20\d\d (.+?) [:A,P:]M/]
   stime.gsub!(/20\d\d /,"")
@@ -64,15 +99,29 @@ while events_string.match(/<item>(.+?)<\/item>/)
   #parse location
   location = description[/\wM (.+?) \wM (.+?)\./]
   location.gsub!(/\wM (.+?) \wM/,"")
-  
+
 
   #add to array
-  temp = Event.new(title,date,0,description,link)
+ # puts title
+  temp = Event.new(title,0,stime, etime,date,0,0,website,description,0)
+  temp.set_info(title,0,stime, etime,date,0,0,website,description,0)
   events_array.push(temp)
   events_string[/<item>(.+?)<\/item>/] = ""
 end
 
 events_array.each do |s|
- #puts s.Title
+  puts "title ", s.Title
+  puts "Location ", s.Location
+  puts "Start ", s.Start
+  puts "End ", s.End
+  puts "Date",s.Date
+  puts "Host",s.Host
+  puts "contact", s.Contact
+  puts "website",s.Website
+  puts "Description",s.Description
+  puts "creator",s.Creator
+  puts " "
+#	query = dbh.prepare("INSERT INTO events values (\'#{s.Title}\', \'#{s.Location}\', \'#{s.Start}\', \'#{s.End}\', \'#{s.Date}\', \'#{s.Host}\', \'#{s.Contact}\', \'#{s.Website}\', \'#{s.Description}\', \'#{s.Creator}\');")
+#	query.execute()
 end
 
